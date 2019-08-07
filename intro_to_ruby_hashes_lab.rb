a56b6ba31452b1b79ba8f948ce9a43c6affddcfe
Steven Laff <steve.laff@gmail.com>
2019-08-07 02:27:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,12 +22,10 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars =>{
-        :pieces_owned =>{
           :one_piece => 25,
           :two_pieces => 50,
           :three_pieces => 100,
           :four_pieces => 200
-        } 
       },
       :names =>{
         :reading_railroad => {},
