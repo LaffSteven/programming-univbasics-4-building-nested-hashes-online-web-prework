b02fccc79421d4bb3d578da16bacd42729e0109f
Steven Laff <steve.laff@gmail.com>
2019-08-07 02:24:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
           :three_pieces => 100,
           :four_pieces => 200
         } 
-      }
+      },
       :names =>{
         :reading_railroad => {},
         :pennsylvania_railroad => {},
